4f877841b148a0139a6eff42b2a010d80a3de8d5
rzssh <reactionnnnn@gmail.com>
2026-08-07 20:43:35 +0200

    feat(input): enable Wooting access

--- modules/hardware/input.nix
+++ modules/hardware/input.nix
@@ -1,4 +1,6 @@
 {
+  hardware.wooting.enable = true;
+
   services.udev.extraRules = ''
     SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1915", ATTRS{idProduct}=="ae1c", MODE="0660", GROUP="input"
     SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1915", ATTRS{idProduct}=="ae11", MODE="0660", GROUP="input"
